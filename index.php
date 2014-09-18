e27d9391721f6f3bc9327404a2e952884957e0b2
Joshua Darlington <joshua_darlington@yahoo.com>
2014-09-17 21:37:46 -0500

    Added rough index text

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>p1</title>
+</head>
+
+<body>
+
+
+<h1>Joshua Darlington</h1> //Your first and last name
+A photo of yourself
+
+<h2>P2</h2>
+Live URL 
+Github URL
+
+<h2>P3</h2>
+Live URL 
+Github URL
+
+<h2>P4</h2>
+Live URL 
+Github URL
+
+Answer the following questions to help the teaching team get to know you:
+
+<h3>In 2+ sentences describe who you are and what you do.</h3>
+I am in the middle of a career change.  I was a professional musician who worked a variety of day jobs in the Los Angeles entertainment industry.  However, I recently moved to the midwest with my wife and child and my skillset is not a good match for the Madison job market.  
+
+<h3>Why are you taking this course?</h3>
+I would like to improve my LAMP skills.  
+
+<h3>What previous experience do you have with programming?</h3>
+I studied Hypercard and Pascal as an undergrad.  I hand coded a number of web 1.0 music web sites and dabbled in various C languages such as C/C++, Ruby, PHP, Java, Javascript, Actionscript, Python, Perl.
+
+<h3>Are you working on a Mac, PC, or some other platform?</h3>
+I am in the process of transitioning from an old PC to a new Mac.
+
+<h3>Anything else we should know?</h3>
+I am interested in the creative uses of data and would appreciate any pointers in that direction.  
+</body>
+
+</html>
