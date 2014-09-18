08a22dfc268c62ef29f10ca12fe38259752b3ba9
Joshua Darlington <joshua_darlington@yahoo.com>
2014-09-18 09:27:09 -0500

    Added bootstrap and photo. Minor text copy revision to bio.

--- index.php
+++ index.php
@@ -1,26 +1,69 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>p1</title>
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" >
+<script src="js/respond.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Bitter:700' rel='stylesheet' type='text/css'>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
 </head>
 
+
+
+
 <body>
+<div class="container">
 
 
-<h1>Joshua Darlington</h1> //Your first and last name
-A photo of yourself
 
+<header>
+	<div class="row">
+<div  class="col-md-12 col-sm-12" >
+
+<h1>Joshua Darlington</h1> 
+</div>
+</div>
+
+<img  alt="Joshua Darlington Headshot" src="Images/Joshua-Darlington-Headshot.jpg" />
+<div class="row">
+<div  class="col-md-12 col-sm-12" >
+</div>
+</div>
+</header>
+
+<nav>
+<div class="row">
+
+<div  class="col-md-4 col-sm-4" >
 <h2>P2</h2>
 Live URL 
 Github URL
+</div>
+
+<div  class="col-md-4 col-sm-4" >
 
 <h2>P3</h2>
 Live URL 
 Github URL
+</div>
 
+
+<div  class="col-md-4 col-sm-4" >
 <h2>P4</h2>
 Live URL 
 Github URL
+</div>
+</div>
+</nav>
+
+
+<article>
+<div class="row">
+<div  class="col-md-12 col-sm-12" >
+
 
 Answer the following questions to help the teaching team get to know you:
 
@@ -31,13 +74,23 @@ I am in the middle of a career change.  I was a professional musician who worked
 I would like to improve my LAMP skills.  
 
 <h3>What previous experience do you have with programming?</h3>
-I studied Hypercard and Pascal as an undergrad.  I hand coded a number of web 1.0 music web sites and dabbled in various C languages such as C/C++, Ruby, PHP, Java, Javascript, Actionscript, Python, Perl.
+I've been programming since about the age of 10, inputing and modifying BASIC games into early personal computers (TSR-80, Atari 400, Apple II and etc.) and mainframes at a local SUNY university where students would help me and let me use their accounts.
+As an undergrad I studied PASCAL at Wayne State University in Detroit and Hypercard at New York University's Interactive Telecommunication Program as part of a two part Computational Media introduction.  I hand coded HTML and Javascript for about a dozen web 1.0 music web sites and dabbled in various C languages such as C/C++, Ruby, PHP, Java, Javascript, Actionscript, Python, Perl.
 
 <h3>Are you working on a Mac, PC, or some other platform?</h3>
 I am in the process of transitioning from an old PC to a new Mac.
 
 <h3>Anything else we should know?</h3>
 I am interested in the creative uses of data and would appreciate any pointers in that direction.  
-</body>
+</div>
+</div>
+</article>
 
+<!-- close container -->
+</div>
+
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
 </html>
