aaf2a1c8e87318c3876437ee45694c5b04285b43
Joshua Darlington <joshua_darlington@yahoo.com>
2014-09-18 11:53:14 -0500

    Code cleanup.  Minor CSS tweeks.

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
 
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <title>p1</title>
-<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" >
-<script src="js/respond.js"></script>
-<link href='http://fonts.googleapis.com/css?family=Lato|Cherry+Cream+Soda|Cookie' rel='stylesheet' type='text/css'>
-<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+			<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" >
+			<script src="js/respond.js"></script>
+			<link href='http://fonts.googleapis.com/css?family=Lato|Cherry+Cream+Soda|Cookie' rel='stylesheet' type='text/css'>
+			<link href="css/style.css" rel="stylesheet" type="text/css" />
+
 </head>
 
 
@@ -20,40 +23,36 @@
 
 
 <header>
-	<div class="row">
-<div  class="col-md-12 col-sm-12" >
-
-<h1>Joshua Darlington</h1> 
-Dynamic Web Apps Portfolio
-</div>
-</div>
-
-<img  alt="Joshua Darlington Headshot" src="Images/Joshua-Darlington-Headshot.jpg" />
 <div class="row">
-<div  class="col-md-12 col-sm-12" >
-</div>
+		<div  class="col-md-12 col-sm-12" >
+		<h1>Joshua Darlington</h1> 
+		Dynamic Web Apps Portfolio
+		<img  alt="Joshua Darlington Headshot" src="Images/Joshua-Darlington-Headshot.jpg" />
+		</div>
 </div>
 </header>
+
+
 
 <nav>
 <div class="row">
 
-<div  class="col-md-4 col-sm-4" >
-<h2>P2</h2>
-View: [link] Github: [link]
-</div>
+		<div  class="col-md-4 col-sm-4" >
+		<h2>P2</h2>
+		View: [link] Github: [link]
+		</div>
 
-<div  class="col-md-4 col-sm-4" >
+		<div  class="col-md-4 col-sm-4" >
 
-<h2>P3</h2>
-View: [link] Github: [link]
-</div>
+		<h2>P3</h2>
+		View: [link] Github: [link]
+		</div>
 
 
-<div  class="col-md-4 col-sm-4" >
-<h2>P4</h2>
-View: [link] Github: [link]
-</div>
+		<div  class="col-md-4 col-sm-4" >
+		<h2>P4</h2>
+		View: [link] Github: [link]
+		</div>
 </div>
 </nav>
 
@@ -62,30 +61,30 @@ View: [link] Github: [link]
 <div class="row">
 <div  class="col-md-12 col-sm-12" >
 
+		<h3>Who are you?</h3>
+		<p>I am in the middle of a career change.  </p>
+
+		<p>I was a professional musician who worked a variety of day jobs in the Los Angeles entertainment industry.  I recently moved to the midwest with my wife and child, and my skillset is not a good match for the Madison job market.  
+
+		<h3>Why DWA?</h3>
+		<p>I would like to improve my LAMP skills.  </p>
+
+		<h3>Code Experience?</h3>
+
+		<p>As an undergrad I studied Hypercard at New York University's Interactive Telecommunication Program as part of a two part Computational Media introduction.  </p>
+
+		<p>I hand coded HTML and Javascript for about a dozen web 1.0 music web sites and dabbled in various C languages such as C/C++, Ruby, PHP, Java, Javascript, Actionscript, Python, Perl.</p>
+
+		<h3>Mac or PC?</h3>
+
+		<p>I am in the process of transitioning from an old PC to a new Mac.</p>
+
+		<h3>Anything else?</h3>
+		<p>I am interested in the creative uses of data and would appreciate any pointers in that direction. </p> 
+		</div>
+		</div>
 
 
-<h3>Who are you?</h3>
-<p>I am in the middle of a career change.  </p>
-
-<p>I was a professional musician who worked a variety of day jobs in the Los Angeles entertainment industry.  I recently moved to the midwest with my wife and child, and my skillset is not a good match for the Madison job market.  
-
-<h3>Why DWA?</h3>
-<p>I would like to improve my LAMP skills.  </p>
-
-<h3>Code Experience?</h3>
-
-<p>As an undergrad I studied PASCAL at Wayne State University in Detroit and Hypercard at New York University's Interactive Telecommunication Program as part of a two part Computational Media introduction.  
-</p>
-<p>
-	I hand coded HTML and Javascript for about a dozen web 1.0 music web sites and dabbled in various C languages such as C/C++, Ruby, PHP, Java, Javascript, Actionscript, Python, Perl.
-</p>
-<h3>Mac or PC?</h3>
-<p>I am in the process of transitioning from an old PC to a new Mac.</p>
-
-<h3>Anything else?</h3>
-<p>I am interested in the creative uses of data and would appreciate any pointers in that direction. </p> 
-</div>
-</div>
 </article>
 
 <!-- close container -->
